18168652a1a823086b6c93e53f277129212e0c60
nepg82 <nepg82@gmail.com>
2026-09-15 20:30:00 -0400

    Add new project URL for OWPOC

--- apps.js
+++ apps.js
@@ -40,6 +40,10 @@ const apps = [
         // description: "Movie Database"
     },
     {
+        url: "https://nepg82.github.io/OWPOC/",
+        // description: "Open World Proof of Concept"
+    },
+    {
         url: "https://nepg82.github.io/Putt-Pad/",
         // description: "Golf score card"
     },
